27a36f09c69d4d39d99e20eadd330bec6f8245d2
Arthur Tristram <103407286+Arthur-0896@users.noreply.github.com>
2026-07-15 23:24:18 +0000

    updated console log to info & added a warning message

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import * as core from "@actions/core";
 async function run() {
   const joke = await getJoke();
   core.info(joke);
+  core.warning("This is a serious joke!!");
   core.setOutput("joke", joke);
 }
 
